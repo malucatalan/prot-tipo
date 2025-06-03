d1c29de3b1d8fef45669b48af3561ba59dc410cd
Alessandro Veloso <alessandroveloso5050@gmail.com>
2025-06-03 17:14:08 -0300

    texto

--- texto_menu_sobre.py
+++ texto_menu_sobre.py
@@ -2,11 +2,11 @@ TEXTOSOBRE = (" O algoritmo de Code e Conta funciona de forma intuitiva"
         " de forma a facilitar o uso infaltil."
         " Como cada pessoa tem uma forma diferente para aprender,"
         " através de menus, é possível navegar a níveis mais baixos"
-        " de menus para especificar os dados e apresentar um conteúdo mais específico para a criança.",
+        " para especificar os dados e apresentar um conteúdo mais específico para a criança.",
         
         " Estes menus solicitam a requisição do usuário e gerenciam "
         "o que será apresentado do conteudo, que é armazenado no dicionário CONTEUDOS. "
-        "Neste dicionario, existe outros que armazenam o conteúdo e exercícios a ser solicitado.",
+        "Neste dicionário, existe outros que armazenam o conteúdo e exercícios a ser solicitado.",
 
         " Assim, de forma colorida e eficiente, nosso projeto garante a solidificação do aprendizado da criança.",
 
